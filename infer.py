b49c2b713860cdc35347e03c6ca1b63901d93078
xander johnson <alexx.johnson@gmail.com>
2014-02-07 17:43:05 -0800

    overhaul + sparse matrix.

--- infer.py
+++ infer.py
@@ -1,81 +1,98 @@
-# -*- coding: utf-8 -*-
 """
-Created on Mon Feb  3 11:53:40 2014
+term-term semantic relationships from co-occurance statistics
 
-@author: xander
+@author: alexander johnson
 """
 
 from __future__ import division
-from collections import OrderedDict
 from nltk.corpus.reader.xmldocs import XMLCorpusReader as XML
 from sklearn.metrics import pairwise_distances
-import numpy as np
 
+from scipy import sparse
+from numpy import matrix
 
 
 # Load the EUbookshop corpus for Maltese
-corpus_root = "EUbookshop/mt"
+corpus_root = "mt"
 mt = XML(corpus_root, ".*.xml")
 
 fileids = [file for file in mt.fileids()]
 files = []
 for file in fileids[:2]:
     print '===== Processing ' + file + ' ====='
-    files.append([word.lower().encode('UTF-8') for word in mt.words(file)])
+    files.append([w.lower().encode('UTF-8') for w in mt.words(file)])
     print 'Processed ' + file
 print "~~~~~~~~~" * 5
 
 # TEST
-test = ['An', 'n-gram', 'tagger', 'is', 'a', 'generalization', 'of', 'a', 'unigram', 'tagger', 'whose', 'context', 'is', 'the', 'current', 'word', 'together', 'with', 'the', 'part-of-speech', 'tags', 'of', 'the', 'n-1', 'preceding', 'tokens,', 'as', 'shown', 'in', '5.9.', 'The', 'tag', 'to', 'be', 'chosen,', 'tn,', 'is', 'circled,', 'and', 'the', 'context', 'is', 'shaded', 'in', 'grey.', 'In', 'the', 'example', 'of', 'an', 'n-gram', 'tagger', 'shown', 'in', '5.9,', 'we', 'have', 'n=3;', 'that', 'is,', 'we', 'consider', 'the', 'tags', 'of', 'the', 'two', 'preceding', 'words', 'in', 'addition', 'to', 'the', 'current', 'word.', 'An', 'n-gram', 'tagger', 'picks', 'the', 'tag', 'that', 'is', 'most', 'likely', 'in', 'the', 'given', 'context.']
+test = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty']
 
 
-def createwindow(words, windowsize):
-    
+def create_window(words, windowsize):
+
+    print 'Creating Window of size', windowsize, '...\n'
+
     # Container & Index
     window = []
     wordindex = 0
-    
-    for w in words:
-        
+    percent = 10
+
+    for w in set(words):
+
+        if percent < ((wordindex / len(words)) * 100):
+            print ((wordindex / len(words)) * 100), '%'
+            percent += 10
+
         # each word needs a window & contexts
         term = [w]
         after = []
         before = []
-        
+
         # iterate though the indicies
         for i in range(wordindex-1, wordindex-1-windowsize, -1):
-        
+
             # index must be above zero or else we the end of the text
             if i >= 0:
                 if i != wordindex:
                     before.append(words[i])
-            
+
         # iterate thorough indicies
         for i in range(wordindex+1, wordindex+1 + windowsize):
-            
+
             #try / except block to deal with indicies out of range
             try:
                 if i != wordindex:
                     after.append(words[i])
             except IndexError:
                 continue
-        
-        # append lists to term and term to windw
+
+        # append lists to term and term to window
         term.append(before)
         term.append(after)
         window.append(term)
-        
+
         # raise index count
-        # note: this normaly wouldn't need to exist but since
-        # words.index(w) wil count the ~first occurane of an item
-        # we need to creae our own counter
+        # note: this normally wouldn't need to exist but since
+        # words.index(w) wil count the ~first occurance of an item
+        # we need to create our own counter
         wordindex += 1
-        
+
     return window
-        
-def ttdictionary(words, window_size):
-    
-    window = createwindow(words, window_size)
+
+
+def get_word_index(window):
+    vectorindex = {}
+    count = 0
+    for word in window:
+        vectorindex[word] = count
+        count += 1
+    return vectorindex
+
+
+def tt_dictionary(words, window_size):
+
+    print '\nCreating Term-Term Dictionary...\n'
+    window = create_window(words, window_size)
     tt = {}
 
     # loop through all of the words
@@ -86,75 +103,102 @@ def ttdictionary(words, window_size):
         for value2 in window:
             term2 = value2[0]
 
-            beforecount = 0
-            aftercount = 0
+            count = 0
 
             # go through the before window and see if
             # there are matches between terms 1 & 2
-
             for potential in value1[1]:
                 if potential == term2:
-                    beforecount += 1
-
-            # add to count, create key if it doesn't exit
-            if tt.has_key((term1, term2, 'before')):
-                tt[(term1, term2, 'before')] += beforecount
-            else:
-                tt[(term1, term2, 'before')] = beforecount
+                    count += 1
 
             # go through the after window and see if
             # there are matches between terms 1 & 2
             for potential in value1[2]:
                 if potential == term2:
-                    aftercount += 1
+                    count += 1
 
             # add to count, create key if it doesn't exist
-            if tt.has_key((term1, term2, 'after')):
-                tt[(term1, term2, 'after')] += aftercount
+            if (term1, term2) in tt.keys():
+                tt[(term1, term2)] += count
             else:
-                tt[(term1, term2, 'after')] = aftercount
+                tt[(term1, term2)] = count
 
     return tt
 
-def ttmatrix(dictionary):
-    matrixlist = []
-    matrixstring = ''
+
+def tt_matrix(dictionary):
+
+    print '\nCreating Term-Term Matrix...\n'
+
+    # this container holds the preprocessed values
+    matrix_list = []
+
     count = 0
-    
-    for k in dictionary.keys():
-        prematrix = []     
-        print count / len(dictionary.keys()), '%'
-        
-        # Get all keys of the same word
-        for match, value in dictionary.items():
-            if match[0] == k[0]:
-                prematrix.append((match,value))
-        
+    percent = 10
 
+    for key, value in dictionary.items():
+
+        if percent < ((count / len(dictionary.items()) * 100)):
+            print (count / len(dictionary.items()) * 100), '%'
+            percent += 10
+
+        #this container holds (context, count)s so we can sort
+        prematrix = []
+        for match, match_value in dictionary.items():
+            if match[0] == key[0]:
+                prematrix.append((match, match_value))
         prematrix.sort()
-        matrixlist.append(prematrix)
-        
-        count +=1
-        
-    # Extract value from matrix list    
-    for term in matrixlist:
-        termstring = ''
-        for context in term:
-            termstring += str(context[1]) + ' '
-            print context
-        termstring += '; '
-        print termstring
-        matrixstring += termstring
-    matrixstring = matrixstring[:-2]
-    return np.matrix(matrixstring)
+        matrix_list.append(prematrix)
+
+        count += 1
+
+    # this string will be the values of the matrix
+    matrix_string = ''
+
+    for term in matrix_list:
+
+        # this is one row's data
+        term_string = ' '
+        for pair in term:
+            term_string += str(pair[1]) + ' '
+        term_string += ' ; '
+        matrix_string += term_string
+
+    # ending ; will break everything so take it out
+    matrix_string = matrix_string[:-2]
+
+    # sparse matrix choice: Compressed Sparse Row Matrix
+    return sparse.csr_matrix(matrix(matrix_string))
 
 
-def ncs(matrix):
-        ncs = 1-pairwise_distances(matrix, metric="cosine")
-        return ncs
-
-
-tt = ttdictionary(test, 4)
-def mapncs(matrix, dictionary):
+def compute_pmi(sparse_matrix):
+   
+    # summation of each row & each column
+    # divide each cell by the product of the two sums
+    # take log of the cell
+    # if its negative, it becomes zero 
+     
     pass
+    
+    
+# baby test
 
+from nltk.corpus import gutenberg
+from nltk.tokenize import wordpunct_tokenize
+raw = gutenberg.raw('austen-emma.txt')
+tokens = wordpunct_tokenize(raw[:500])
+d = tt_dictionary(tokens, 2)
+m = tt_matrix(d)
+
+
+
+# i can try linalg.svd but its probably slow
+
+# divisi - using C for SVD
+
+# MDP - modular data processing toolkit for python
+# algorithmnic complexity
+
+# PMI & SVD is the goal right now
+
+# test on 100K
