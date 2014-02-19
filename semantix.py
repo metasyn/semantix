8bce1c5228b0e0e629fff98c4e002f7c1e3b5407
xander johnson <alexx.johnson@gmail.com>
2014-02-18 19:14:39 -0800

    divisi addition for SVD

--- semantix.py
+++ semantix.py
@@ -1,59 +1,68 @@
-#from __future__ import division
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 15:09:52 2014
+
+@author: xander
+"""
 import re
 from nltk.probability import FreqDist
 from scipy.sparse import lil_matrix
-from numpy import log
-from scipy.linalg import svd
-from scipy.spatial.distance import cosine
-
-
+import divisi2
+from numpy import log as npl
 
 
 # open the estonian open subtitles corpus
 # strip non-alpha characters
 # set case to lower
 
-infile = open("500k.txt", "r")
+infile = open("walden.txt", "r")
 raw = infile.read()
 infile.close()
 
 # define parameters
 windowSize = 1
-Nwords = 40000
-Ncontext = 2000
+Nwords = 4000
+Ncontexts = 200
 
-# terms = columns, context = rows (dimensions of the matrix)
+print "Processing text..."
+# terms = columns, contexts = rows (dimensions of the matrix)
 words = [w.lower() for w in raw.split() if re.match(ur"^[^\W\d_]+$", w)][:100000]
 vocab = FreqDist(words)
 terms = vocab.keys()[:Nwords]
-context = vocab.keys()[:Ncontext]
+contexts = vocab.keys()[:Ncontexts]
 N = len(words)
 
+print "Creating count matrix..."
 # create sparse matrix
-m = lil_matrix((Nwords, Ncontext))
-
+m = lil_matrix((Nwords, Ncontexts))
 for i in range(2, N-2):
     # progress check
-    if i % 10000 == 0:
+    if i % 1000 == 0:
         print i
     # loop through windows, add to matrix
     if words[i] in terms:
         indices = [i-2,i-1,i+1,i+2]
         for j in indices:
-            if words[j] in context:
-                m[terms.index(words[i]),context.index(words[j])] += 1.
+            if words[j] in contexts:
+                m[terms.index(words[i]),contexts.index(words[j])] += 1.
 
-# point wise mutual information
+print "== Generating +PMI =="
+print "Normalizing..."
+
 m = m/m.sum()
 column_sum = m.sum(0)
 row_sum = m.sum(1)
 denominator = lil_matrix(row_sum.dot(column_sum))
 m = m/denominator
-m[m!=0] = log(m[m!=0])
-# only need positive PMI 
+
+print "Getting log..."
+m[m!=0] = npl(m[m!=0])
 m[m < 0] = 0.
 
-# scipy svd
-U, s, Vh = svd(m.A, full_matrices=False)
+print " == SVD =="
+divisi_sparse = divisi2.SparseMatrix(m.A, row_labels=terms, col_labels=contexts)
+U, S, V = divisi_sparse.svd(k=100)
 
-
+# two, three million in english
+# compute nearest neighbors or cosine distance
+# magnus sahlgren
